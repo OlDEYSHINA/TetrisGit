5b7e49a5f777d427aa50facd3a2fe2b5eaa18e25
Андрей <@DEXP>
2020-02-01 19:00:54 +0700

    Добавил фигуру, надо доделать удаление линии

--- Controller.cs
+++ Controller.cs
@@ -33,14 +33,24 @@ namespace Tetris2
         }
         public static void MoveLeft()
         {
+            Console.WriteLine("Влево");
+           /* if ()
             if (Tetris2.Program.Place[Program.ActiveAction[0, 1], Program.ActiveAction[0, 0]] == 0)
             {
-
-            }
+                break;
+            }*/
+            Tetris2.Program.ActiveAction[0, 0]--;
+            Tetris2.Program.ActiveAction[1, 0]--;
+            Tetris2.Program.ActiveAction[2, 0]--;
+            Tetris2.Program.ActiveAction[3, 0]--;
         }
         public static void MoveRight()
         {
-            
+            Console.WriteLine("Вправо");
+            Tetris2.Program.ActiveAction[0, 0]++;
+            Tetris2.Program.ActiveAction[1, 0]++;
+            Tetris2.Program.ActiveAction[2, 0]++;
+            Tetris2.Program.ActiveAction[3, 0]++;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace Tetris2
 
     class Program
     {
+        static public int[,] OldAction = new int[4, 2];
         static public int[,] ActiveAction = new int[4, 2];  //Координаты активной фигуры
         static public Byte[,] Place = new Byte[23, 10];
         static int Figure;                          // Переменная случайной генерации номера фигуры
@@ -21,6 +22,7 @@ namespace Tetris2
             NeedSpawnForm = true;
             while (true)
             {
+                SaveOldPosition();
                 DrawField();
                 Controller.Control();
                 if (NeedSpawnForm == true)
@@ -31,7 +33,8 @@ namespace Tetris2
                 {
                     DeleteOldForm();
                     FallForm();
-                    DrawForm();
+                    if(Place[ActiveAction[0,1],ActiveAction[0,0]]!=1) DrawForm();
+
                 }
                 if (Console.ReadKey(true).Key == ConsoleKey.Escape)
                 {
@@ -64,9 +67,9 @@ namespace Tetris2
                     {
                         if (Tetris2.Program.Place[i, j] == 1)
                         {
-                            Console.Write(" □");
+                            Console.Write(" H");
                         }
-                        else Console.Write(" *");
+                        else Console.Write(" O");
 
                     }
 
@@ -82,16 +85,26 @@ namespace Tetris2
         }
         public static void DeleteOldForm()
         {
-            Place[ActiveAction[0, 1], ActiveAction[0, 0]] = 0;
-            Place[ActiveAction[1, 1], ActiveAction[1, 0]] = 0;
-            Place[ActiveAction[2, 1], ActiveAction[2, 0]] = 0;
-            Place[ActiveAction[3, 1], ActiveAction[3, 0]] = 0;
+            Place[OldAction[0, 1], OldAction[0, 0]] = 0;
+            Place[OldAction[1, 1], OldAction[1, 0]] = 0;
+            Place[OldAction[2, 1], OldAction[2, 0]] = 0;
+            Place[OldAction[3, 1], OldAction[3, 0]] = 0;
+        }
+
+        public static void SaveOldPosition()
+        {
+            for (int i = 0; i <= 3; i++)
+			{
+                OldAction[i,0]=ActiveAction[i,0];
+                OldAction[i,1]=ActiveAction[i,1];
+			}
+            
         }
 
         public static void FallForm()
         {
             Console.Write(ActiveAction[2, 1]);
-            if (ActiveAction[2, 1] != 22 && ActiveAction[2, 0] != 22 && Place[ActiveAction[2, 1] + 1, ActiveAction[2, 0]] == 0 & Place[ActiveAction[3, 1] + 1, ActiveAction[3, 0]] == 0)
+            if (ActiveAction[3, 1] != 22 && ActiveAction[0, 1] != 22 && ActiveAction[1, 1] != 22 && ActiveAction[2, 0] != 22 && Place[ActiveAction[2, 1] + 1,ActiveAction[2, 0]] == 0 && Place[ActiveAction[3, 1] + 1, ActiveAction[3, 0]] == 0)
             {
                 ActiveAction[0, 1]++;
                 ActiveAction[1, 1]++;
@@ -103,26 +116,46 @@ namespace Tetris2
             {
                 NeedSpawnForm = true;
                 DrawStaticForm();
+                CheckFullLine();
+            }
+        }
+        public static void CheckFullLine()
+        {
+            int ChekingLine = ActiveAction[0,1] ;
+            int FullInLine = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (Place[ChekingLine,i]==1)
+                {
+                    FullInLine++;
+                }
+            }
+            if (FullInLine==10)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Place[ChekingLine, i] = 0;
+                }
             }
         }
         public static void DrawStaticForm()
         {
-            Place[ActiveAction[0, 1], ActiveAction[0, 0]] = 2;
-            Place[ActiveAction[1, 1], ActiveAction[1, 0]] = 2;
-            Place[ActiveAction[2, 1], ActiveAction[2, 0]] = 2;
-            Place[ActiveAction[3, 1], ActiveAction[3, 0]] = 2;
+            Place[ActiveAction[0, 1], ActiveAction[0, 0]] = 1;
+            Place[ActiveAction[1, 1], ActiveAction[1, 0]] = 1;
+            Place[ActiveAction[2, 1], ActiveAction[2, 0]] = 1;
+            Place[ActiveAction[3, 1], ActiveAction[3, 0]] = 1;
         }
 
         public  static void SpawnForm()
         {
-            Tetris2.Program.Figure = Rand.Next(0, 6);
+            Tetris2.Program.Figure = Rand.Next(0, 2);
             switch (Figure)
             {
                 case 0: //Квадрат
-                    ActiveAction[0, 0] = 4; // Y koordinata
-                    ActiveAction[0, 1] = 2;
+                    ActiveAction[0, 0] = 4; // Y koordinata             ОТЧЕТ ИДЕТ СЛЕВА НАПРАВО ПО Y СВЕРХУ ВНИЗ ПО Х
+                    ActiveAction[0, 1] = 2; // X Верхняя Левая
                     ActiveAction[1, 0] = 5; // Y
-                    ActiveAction[1, 1] = 2;
+                    ActiveAction[1, 1] = 2; // X Вернхняя Правая
                     ActiveAction[2, 0] = 4; // Y
                     ActiveAction[2, 1] = 3; // X Нижняя Левая
                     ActiveAction[3, 0] = 5; // Y
@@ -131,9 +164,9 @@ namespace Tetris2
                     NeedSpawnForm = false;
                     break;
                 case 1:
-                    ActiveAction[0, 0] = 4;
-                    ActiveAction[0, 1] = 2;
-                    ActiveAction[1, 0] = 5;
+                    ActiveAction[0, 0] = 3;
+                    ActiveAction[0, 1] = 3;
+                    ActiveAction[1, 0] = 4;
                     ActiveAction[1, 1] = 2;
                     ActiveAction[2, 0] = 4;
                     ActiveAction[2, 1] = 3;
@@ -143,14 +176,14 @@ namespace Tetris2
                     NeedSpawnForm = false;
                     break;
                 case 2:
-                    ActiveAction[0, 0] = 4;
+                    ActiveAction[0, 0] = 3;
                     ActiveAction[0, 1] = 2;
-                    ActiveAction[1, 0] = 5;
-                    ActiveAction[1, 1] = 2;
+                    ActiveAction[1, 0] = 3;
+                    ActiveAction[1, 1] = 3;
                     ActiveAction[2, 0] = 4;
                     ActiveAction[2, 1] = 3;
-                    ActiveAction[3, 0] = 5;
-                    ActiveAction[3, 1] = 3;
+                    ActiveAction[3, 0] = 4;
+                    ActiveAction[3, 1] = 4;
                     DrawForm();
                     NeedSpawnForm = false;
                     break;
@@ -162,10 +195,10 @@ namespace Tetris2
         }
         public static void DrawForm()
         {
-            Place[ActiveAction[0, 1], ActiveAction[0, 0]] = 1;
-            Place[ActiveAction[1, 1], ActiveAction[1, 0]] = 1;
-            Place[ActiveAction[2, 1], ActiveAction[2, 0]] = 1;
-            Place[ActiveAction[3, 1], ActiveAction[3, 0]] = 1;
+            Place[ActiveAction[0, 1], ActiveAction[0, 0]] = 2;
+            Place[ActiveAction[1, 1], ActiveAction[1, 0]] = 2;
+            Place[ActiveAction[2, 1], ActiveAction[2, 0]] = 2;
+            Place[ActiveAction[3, 1], ActiveAction[3, 0]] = 2;
         }
       
 
